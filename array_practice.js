305203701ae44e5595e7b3420ed37467d9fef607
Garret Beebe <garret.beebe@gmail.com>
2023-02-26 21:02:40 -0800

    cleaning up unecessary variable assignments and fixing some scoping issues.

--- array_practice.js
+++ array_practice.js
@@ -7,19 +7,16 @@
 
 
 function init(length, number_missing) {
-    const arr = Array.from({length:length}, (_, index) => index +1);
+    arr = Array.from({length:length}, (_, index) => index +1);
     for (i = 0; i < number_missing; i++) {
-        var element = Math.floor(Math.random() * length) + 1;
-        delete arr[element];
+        delete arr[Math.floor(Math.random() * length) + 1];
     }
     console.log(arr);
-    return arr;
 }
 
 function FindMissingElements(arr) {
-    var current_element = 0;
-    var missing_elements = [];
-    var missing_value = 0;
+    let current_element;
+    let missing_elements = [];
 
     for (j = 0; j < arr.length; j++) {
         if (arr[j] === '' || arr[j] === undefined) { 
@@ -29,14 +26,14 @@ function FindMissingElements(arr) {
     }
 
     for (k = 0; k < missing_elements.length; k++) {
-        missing_value = arr[missing_elements[k] - 1] + 1;
         console.log ("Missing element is: " + missing_elements[k]);
-        console.log ("Mising value is: " + missing_value);
+        console.log ("Mising value is: " + (arr[missing_elements[k] - 1] + 1));
     }
     
 }
 
 const args = process.argv;
 console.log(args);
-const arr = init(args[2], args[3]);
+var arr;
+init(args[2], args[3]);
 FindMissingElements(arr);
