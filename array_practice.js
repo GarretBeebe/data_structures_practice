a99e6937ed7852b4882b08f7d5c1ca8769188e76
Garret Beebe <garret.beebe@gmail.com>
2023-02-26 21:21:20 -0800

    working on an edge case

--- array_practice.js
+++ array_practice.js
@@ -5,35 +5,37 @@
         node array_practice.js length number_missing
 */
 
+var ARGS = process.argv;
+var ARR;
 
-function init(length, number_missing) {
-    arr = Array.from({length:length}, (_, index) => index +1);
-    for (i = 0; i < number_missing; i++) {
-        delete arr[Math.floor(Math.random() * length) + 1];
+function init() {
+    let length = ARGS[2];
+    let missingElements = ARGS[3]
+    ARR = Array.from({length:length}, (_, index) => index +1);
+    for (let i = 0; i < missingElements; i++) {
+        delete ARR[Math.floor(Math.random() * length) + 1];
     }
-    console.log(arr);
+    console.log(ARR);
 }
 
-function FindMissingElements(arr) {
-    let current_element;
-    let missing_elements = [];
+function findMissingElements() {
+    let currentElement;
+    let missingElements = [];
 
-    for (j = 0; j < arr.length; j++) {
-        if (arr[j] === '' || arr[j] === undefined) { 
-            current_element = j;
-            missing_elements.push(j);
+    for (let i = 0; i < ARR.length; i++) {
+        if (ARR[i] === '' || ARR[i] === undefined) { 
+            currentElement = i;
+            missingElements.push(i);
         }
     }
 
-    for (k = 0; k < missing_elements.length; k++) {
-        console.log ("Missing element is: " + missing_elements[k]);
-        console.log ("Mising value is: " + (arr[missing_elements[k] - 1] + 1));
+    for (let i = 0; i < missingElements.length; i++) {
+        console.log ("Missing element is: " + missingElements[i]);
+        console.log ("Mising value is: " + (ARR[missingElements[i] - 1] + 1));
     }
     
 }
 
-const args = process.argv;
-console.log(args);
-var arr;
-init(args[2], args[3]);
-FindMissingElements(arr);
+
+init();
+findMissingElements();
