21e2bb2807b73a9ece5a837952b16aa8dfa55e89
Garretson Beebe <garret@TK-421-Mac-Pro.local>
2023-02-27 16:40:02 -0800

    changed each loop to find multiple occurances of the passed search parameter.

--- JSON String Example/json_string_example.js
+++ JSON String Example/json_string_example.js
@@ -1,17 +1,24 @@
 /* 
     Find a key and value in a JSON Object based on a provided value
 */
-
 var _ = require('underscore');
 var flatten = require('flat')
-var ARGS = process.argv;
-var json = require(process.cwd() + '/generated.json');
-var flatObj = flatten(json);
-console.log(flatten(json));
 
-_.each(flatObj, function(value, key){
-    if (value === ARGS[2]) {
+var args = process.argv;
+var json = require(process.cwd() + '/generated.json');
+var flatJSON = flatten(json);
+var keys = [];
+
+_.each(flatJSON, function(value, key){
+    let obj = json;
+    let test = String(value);
+    if (test.includes(args[2])===true) {
         console.log(key + " : " + value);
+        keys = key.split(".");
+        for (let i = 0; i < keys.length; i++) {
+            obj = obj[keys[i]];
+        }
+        console.log(obj);
     }
 });
 
