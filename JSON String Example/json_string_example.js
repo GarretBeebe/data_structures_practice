4cd892617c70cb7d15423691b70ef8d83d8fad9c
Garret Beebe <garret.beebe@gmail.com>
2023-02-27 21:01:11 -0800

    adding a readme and cleaning comments

--- JSON String Example/json_string_example.js
+++ JSON String Example/json_string_example.js
@@ -1,5 +1,7 @@
 /* 
-    Find a key and value in a JSON Object based on a provided value
+    Find a key and value in a JSON Object based on a provided value.
+    To run:
+        node json_string_example.js 'your search string'
 */
 var _ = require('underscore');
 var flatten = require('flat');
---
+++ README.md
@@ -0,0 +1,8 @@
+## Data Structures Practice
+    Practice repository for data structures an algorithims technical interviews.
+
+### Setup
+    - Install Node 10.8.0 (can use newer versions, I was just lazy)
+    - Install npm 6.2.0
+    - Within each folder run:
+      - `npm install`
