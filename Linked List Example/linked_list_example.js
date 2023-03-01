5990e7ade3c518cff34c4e83fc6fd3ac14d62c5a
Garretson Beebe <garret@TK-421-Mac-Pro.local>
2023-03-01 09:54:19 -0800

    still refining the midpoint count and depth of linked list created

--- Linked List Example/linked_list_example.js
+++ Linked List Example/linked_list_example.js
@@ -1,31 +1,53 @@
 /*
  Find midpoint of a linked list (in this case, a JSON object)
+ to run:
+    node linked_list_example.js 'depth of linked list'
  */
 
-
+var args = process.argv;
 var flatten = require('flat');
 
-const list = {
-    head: {
-        value: 6,
-        next: {
-            value: 10,                                             
-            next: {
-                value: 12,
-                next: {
-                    value: 3,
-                    next: null	
-                    }
-                }
-            }
-        }
-    };
-
-var obj = flatten(list);
-var midPoint = Math.floor(Object.keys(flatten(list)).length/2)
-var keys = Object.keys(obj)[midPoint].split(".");
-obj = list;
-for (let i = 0; i < keys.length-1; i++) {
-    obj = obj[keys[i]];
+class Node {
+    constructor(data, next = null){
+        this.data = data,
+        this.next = next
+    }
 }
-console.log(obj.value);
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+}
+
+function populateLinkedList () {
+    let data;
+    let next;
+    for (i=0; i<length; i++) {
+        data = Math.floor(Math.random() * 100);
+        next = list.head;
+        list.head = new Node(Math.floor(Math.random() * 100), list.head);
+    }
+}
+
+function findMidPoint () {
+    let obj = flatten(list);
+    console.log(obj);
+    let midPoint = Math.floor(Object.keys(obj).length/2)
+    console.log(midPoint);
+    let keys = Object.keys(obj)[midPoint].split(".");
+    console.log(keys);
+    obj = list;
+    for (let i = 0; i < keys.length-1; i++) {
+        obj = obj[keys[i]];
+    }
+    console.log(obj.data);
+}
+
+
+var length = args[2];
+var list = new LinkedList();
+populateLinkedList();
+findMidPoint();
+console.log(list);
+
