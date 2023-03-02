b7e9bf7dc678123ccedd41b11f58e4b0f9ad3b62
Garretson Beebe <garret@TK-421-Mac-Pro.local>
2023-03-02 08:35:56 -0800

    adding remove node

--- Binary Search Tree/binary_search.js
+++ Binary Search Tree/binary_search.js
@@ -1,7 +1,9 @@
 /* Simple Binary Tree insert and find
     to run:
-        node binary_search.js number_of_nodes value_to_find
+        node binary_search.js number_of_nodes value_to_find value_to_delete
 */
+
+const { isNull } = require("underscore");
 
 class Node {
     constructor(value){
@@ -18,11 +20,14 @@ class Node {
 
     insert(value){
         var newNode = new Node(value);
+
         if(this.root === null){
             this.root = newNode;
             return this;
         }
+
         let current = this.root;
+
         while(current){
             if(value === current.value) return undefined;
             if(value < current.value){
@@ -46,6 +51,7 @@ class Node {
         
         let current = this.root
         let found = false
+
         while(current && !found){
               if(value < current.value){
                 current = current.left
@@ -62,6 +68,73 @@ class Node {
         
     
     }
+
+    remove(value){
+        this.root = this.removeNode(this.root, value)
+    }
+    // a recursive function to insert a new value in binary search tree
+    
+    removeNode(current, value) {
+        
+        // base case, if the tree is empty 
+        
+       if(current === null) return current
+        
+        // when value is the same as current's value, this is the node to be deleted
+        
+        if (value === current.value) {
+             
+            // for case 1 and 2, node without child or with one child
+            
+            if (current.left === null && current.right === null){
+                
+                    return null
+                
+                }else if(current.left === null){
+                
+                    return current.right 
+             
+                }else if(current.right === null){
+                
+                    return current.left
+                
+                }else{
+                    
+                    /// node with two children, get the inorder successor, 
+                    //smallest in the right subtree
+                    
+                    let tempNode = this.kthSmallestNode(current.right)
+                        current.value = tempNode.value
+                    
+                    /// delete the inorder successor
+                    
+                        current.right = this.removeNode(current.right, tempNode.value)
+                    return current
+            }
+
+        // recur down the tree
+            
+        }else if(value < current.value) {
+            
+            current.left = this.removeNode(current.left, value)
+            return current
+            
+        }else{
+            
+            current.right = this.removeNode(current.right, value)
+            return current
+        }
+    }
+    
+     /// helper function to find the smallest node
+    
+    kthSmallestNode(node) {
+        while(!node.left === null)
+            node = node.left
+
+        return node
+    }
+
   }
 
   // Testing insert and find using simple insert values
@@ -74,3 +147,8 @@ class Node {
 
   console.log(myTree);
   console.log(myTree.find(args[3]));
+
+  if(args[4]) {
+    myTree.remove(args[4]);
+    console.log(myTree);
+  }
