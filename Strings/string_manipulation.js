1606bd36e41e79c3466e9f9e017c456e5ace90e4
Garretson Beebe <garret@TK-421-Mac-Pro.local>
2023-03-02 09:29:59 -0800

    adding comments

--- Strings/string_manipulation.js
+++ Strings/string_manipulation.js
@@ -1,7 +1,19 @@
+/*
+    You have two strings. Both strings are series of numbers.
+    Implement a function to check whether the second string is a valid pop sequence of the first string.
+
+    Ex.
+    "12345" "32145"
+    "12345" "54321"
+    "12345" "21543"
+*/
+
+
 var originalString = "12345"
 var string2 = "32145"
 var string3 = "54321"
 var string4 = "21543"
+var stringArray = [string2, string3, string4];
 
 var array1 = originalString.split("");
 
@@ -19,7 +31,6 @@ function check (string) {
 
 }
 
-var stringArray = [string2, string3, string4];
 
 for (i=0; i<stringArray.length; i++) {
     console.log(stringArray[i] + " : " + check(stringArray[i]));
